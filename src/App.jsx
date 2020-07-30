9f5c7db9c0f74b5d61b7fd2ce3b3be9233a432fc
Marat Dospolov <marat.dospolov@gmail.com>
2020-07-30 05:29:44 +0300

    fontsize changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Patron from 'cmp/Patron'
 
 import './App.css'
 
-const patrons = ['bill gates', 'elon musk', 'steve jobs', 'john doe', 'ross geller']
+const patrons = [
+  'bill gates junior the second',
+  'elon',
+  'd420gsdjg2020jg2gj',
+  'sam.jones@outlook.com',
+  'ross geller'
+]
 
-const App = () => (
-  <div className="patron-panel">
-    {patrons.map(patron => (
-      <Patron {...{ patron }} />
-    ))}
-  </div>
-)
+const App = () => {
+  const [fontSize, setFontSize] = useState(10)
+  const [visibility, setVisibility] = useState('hidden')
+  const patronPanelEl = useRef(null)
+  const patronPanelContentEl = useRef(null)
+
+  useEffect(() => {
+    const parentHeight = patronPanelEl.current.offsetHeight
+    const contentHeight = patronPanelContentEl.current.offsetHeight
+    if (parentHeight < contentHeight) {
+      setFontSize(prevFontSize => prevFontSize - 0.3)
+    } else {
+      setVisibility('visible')
+    }
+  })
+
+  return (
+    <div
+      className="patron-panel"
+      ref={patronPanelEl}
+      style={{ padding: `${fontSize / 4}rem` }}
+    >
+      <div
+        className="patron-panel-content"
+        ref={patronPanelContentEl}
+        style={{ visibility }}
+      >
+        {patrons.map((patron, i) => (
+          <Patron key={i} {...{ patron, fontSize, visibility }} />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default App
--- src/components/Patron.jsx
+++ src/components/Patron.jsx
@@ -1,3 +1,16 @@
 import React from 'react'
+import { Avatar } from 'antd'
 
-export default ({ patron }) => <span className="patron">{patron}</span>
+export default ({ patron, fontSize, visibility }) => (
+  <span
+    className="patron"
+    style={{
+      visibility,
+      fontSize: `${fontSize}rem`,
+      margin: `${fontSize / 10}rem`,
+      padding: `${fontSize / 5}rem ${fontSize / 2}rem`
+    }}
+  >
+    <Avatar size={fontSize * 10}>{patron}</Avatar> {patron}
+  </span>
+)
